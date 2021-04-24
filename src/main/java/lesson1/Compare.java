1e1af69e8748791af15cb03da70db68faefa3977
Vekyork <vekyork@yandex.ru>
2021-04-24 21:33:02 +0300

    lesson1.Compare

--- src/main/java/lesson1/Compare.java
+++ src/main/java/lesson1/Compare.java
@@ -15,9 +15,9 @@ public class Compare {
         String Dog2 = "Boris";
         String[] dogs1 = {"Шарик", "Барс", "Рыжик", "Тузик", "Лайка"};
         String[] dogs2 = {"Вулкан", "Барбос", "Пират", "Буран", "Силач"};
-        Animal Dog = new Animal("Собака", "Рыжик", 3);  //переменная класса Animal
+        Animal Dog = new Animal("Животные", "Рыжик", 3);  //переменная класса Animal
         int[] arr1 = {1, 2, 3, 4, 5, 8, 12, 24, 33, 15, 16, 17}; //для сравнения вышеперечисленных переменных с другими переменными методом перебора создаем массивы
-        int[] arr2 = {1, 2, 4, 22, 33, 44, 33, 16, 19};  //в одном массиве искомая переменная есть, в другом нет
+        int[] arr2 = {2, 4, 22, 33, 44, 33, 16, 19};  //в одном массиве искомая переменная есть, в другом нет
         System.out.println("a= " +a);
         System.lineSeparator();
         System.out.println("b= " +b);
@@ -32,10 +32,34 @@ public class Compare {
         System.lineSeparator();
 
         long nano_startTime = System.nanoTime();
-        for (int i = 0; i < arr1.length; i++) {
-            String strToPrint = "arr1[" + i + "]=" + arr1[i];
-        }
+//        for (int i = 0; i < arr1.length; i++) {
+//            String strToPrint = "arr1[" + i + "]=" + arr1[i];
+//        }
+        search(a, arr1); //число есть
+        search(a, arr2); //числа нет
+        search(Dog1, dogs1); //животное есть
+        search(Dog1, dogs2);//животного нет
         long nano_endTime = System.nanoTime() - nano_startTime;
         System.out.println("Время выполнения сравнения, нс: " + nano_endTime);
+    }
+    //создаем оверлоад методы для сравнения переменных
+    public static void search(int a, int[] arr1) {
+        for (int arr : arr1) {
+            if (a == arr) {
+                System.out.printf("Число %d присутствует в приведенном массиве чисел\n", a);
+                return;
+            }
+        }
+        System.out.printf("Число %d отсутствует в приведенном массиве чисел\n", a);
+    }
+
+        public static void search(String a, String[] words) {
+        for (String word : words) {
+            if (a.equals(word)) {
+                System.out.printf("Слово %s присутствует в приведенном массиве слов\n", a);
+                return;
+            }
+        }
+        System.out.printf("Слово %s отсутствует в приведенном массиве слов\n", a);
     }
 }
